303998949eb06b0b278cf951abc86921f4c661a0
weiyx <2997053342@qq.com>
2020-07-21 18:21:56 +0800

    导出竞价时，供应商名称问题

--- galaplat-comprehensive-bidding-dao/src/main/java/com/galaplat/comprehensive/bidding/dao/dvos/BidDVO.java
+++ galaplat-comprehensive-bidding-dao/src/main/java/com/galaplat/comprehensive/bidding/dao/dvos/BidDVO.java
@@ -1,7 +1,14 @@
 package com.galaplat.comprehensive.bidding.dao.dvos;
 
+import lombok.*;
+
 import java.math.BigDecimal;
 
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BidDVO {
     private String code;
     /*竞品id*/
@@ -15,63 +22,4 @@ public class BidDVO {
 
     private String createdTime;
 
-    public String getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setCreatedTime(String createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    @Override
-    public String toString() {
-        return "Bid{" +
-                "code='" + code + '\'' +
-                ", goodsId=" + goodsId +
-                ", userCode='" + userCode + '\'' +
-                ", activityCode='" + activityCode + '\'' +
-                ", bid=" + bid +
-                ", createdTime='" + createdTime + '\'' +
-                '}';
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public Integer getGoodsId() {
-        return goodsId;
-    }
-
-    public void setGoodsId(Integer goodsId) {
-        this.goodsId = goodsId;
-    }
-
-    public String getUserCode() {
-        return userCode;
-    }
-
-    public void setUserCode(String userCode) {
-        this.userCode = userCode;
-    }
-
-    public String getActivityCode() {
-        return activityCode;
-    }
-
-    public void setActivityCode(String activityCode) {
-        this.activityCode = activityCode;
-    }
-
-    public BigDecimal getBid() {
-        return bid;
-    }
-
-    public void setBid(BigDecimal bid) {
-        this.bid = bid;
-    }
 }
