c35ab0e65af3caab3bc9c8ca98d18a8b5f1f42b4
yangmingsen <1710644559@qq.com>
2020-09-17 14:46:07 +0800

    增加实际竞标日逻辑

--- galaplat-comprehensive-bidding-service/src/main/java/com/galaplat/comprehensive/bidding/controllers/AdminController.java
+++ galaplat-comprehensive-bidding-service/src/main/java/com/galaplat/comprehensive/bidding/controllers/AdminController.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -236,6 +237,7 @@ public class AdminController extends BaseController {
                         final ActivityDO tActivity = new ActivityDO();
                         tActivity.setCode(activityCode);
                         tActivity.setStatus(3);
+                        tActivity.setPracticalBidTime(new Date());
 
                         activityService.updateByPrimaryKeySelective(tActivity);
                     }
